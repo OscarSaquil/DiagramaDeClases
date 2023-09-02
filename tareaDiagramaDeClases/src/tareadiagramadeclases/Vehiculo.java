015e078f5a8f72337c15d971a8217a5922c820ef
OscarSaquil <128245789+OscarSaquil@users.noreply.github.com>
2023-09-01 18:53:28 -0600

    Add files via upload

---
+++ tareaDiagramaDeClases/src/tareadiagramadeclases/Vehiculo.java
@@ -0,0 +1,16 @@
+package tareadiagramadeclases;
+
+/**
+ *
+ * @author oscar
+ */
+public class Vehiculo implements objetoInerte,Actor{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ tareaDiagramaDeClases/src/tareadiagramadeclases/bicicleta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tareadiagramadeclases;
+
+/**
+ *
+ * @author oscar
+ */
+public class bicicleta extends Vehiculo{
+    public class Bicileta{
+        private String Marca;
+        private int marchas;
+    }
+}
